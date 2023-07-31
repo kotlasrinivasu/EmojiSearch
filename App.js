5588008a4673dac5b39a58fe1f263004581819ce
kotlasrinivasu <121429003+kotlasrinivasu@users.noreply.github.com>
2023-07-31 12:29:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+//import NameList from './components/NameList';//
+import Emoji from './components/Emoji';
+function App() {
+  return (
+    <div className="App">
+  
+  <Emoji />
+      
+    </div>
+  );
+}
+
+export default App;
